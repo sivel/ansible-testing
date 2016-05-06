411925f405361d9dc33666043df88cfa0cbb8483
Matt Martz <matt@sivel.net>
2016-05-06 10:37:54 -0500

    flake8 cleanup

--- setup.py
+++ setup.py
@@ -17,11 +17,11 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import codecs
 import os
 import re
-import codecs
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
